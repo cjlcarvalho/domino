fe6ec541d033a84c7651ee9215436cff490c0a0a
Caio Carvalho <caiojcarvalho@gmail.com>
2018-04-03 14:13:09 -0300

    Including QApplication icon.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 
 #include <QApplication>
 #include <QDebug>
+#include <QIcon>
+#include <QPixmap>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    a.setWindowIcon(QIcon(QPixmap(":/images/domino.png")));
 
     MainDialog md;
     md.exec();
